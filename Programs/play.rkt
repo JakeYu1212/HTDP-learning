29a86b25f43da47df3b45ffd29c9c7444391a177
Gen.Yu <yugen_1212@163.com>
2019-03-08 19:25:23 +0800

    big-bang

--- Programs/play.rkt
+++ Programs/play.rkt
@@ -2,4 +2,53 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname play) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (require 2htdp/batch-io)
+(require 2htdp/image)
+(require 2htdp/universe)
 ;(read-file 'stdin)
+
+;(write-file "sample.txt" "212")
+;(write-file 'stdout "212\n")
+;(read-file "sample.txt")
+;(read-file 'stdin)
+(define (C f)
+  (* 5/9 (- f 32)))
+
+(define (convert in out)
+  (write-file out
+    (string-append
+      (number->string
+        (C
+          (string->number
+            (read-file in))))
+      "\n")))
+
+;(convert "sample.txt" 'stdout)
+
+(define (number->square s)
+  (square s "solid" "red"))
+
+(define (reset s ke)
+  100)
+
+
+(define BACKGROUND (empty-scene 100 100))
+(define DOT (circle 3 "solid" "red"))
+
+(define (main y)
+  (big-bang y
+    [on-tick sub1]
+    [stop-when zero?]
+    [to-draw place-dot-at]
+    [on-key stop]))
+
+(define (place-dot-at y)
+  (place-image DOT 50 y BACKGROUND))
+
+(define (stop y ke)
+  0)
+
+
+
+
+
+
